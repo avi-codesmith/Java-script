531731af8df3140fcb1a622a4578044e89e04122
avi-codesmith <avi.codesmith@gmail.com>
2024-12-30 21:38:14 +0530

    Write About Objects and Arrays

--- Section3/lecture-29/script.js
+++ Section3/lecture-29/script.js
@@ -13,3 +13,24 @@ console.log(jonas["lastName"]);
 const nameKey = "Name";
 console.log(jonas["first" + nameKey]);
 console.log(jonas["last" + nameKey]);
+
+const interestedIn = prompt(
+  "What you want to know about Jonas? choose any one option Firstname, Lastname, Age, Job, Friends."
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Something went wrong, Choose option again");
+  prompt(
+    "What you want to know about Jonas? choose any one option Firstname, Lastname, Age, Job, Friends."
+  );
+}
+
+jonas.location = "portugal";
+jonas["twitter"] = "@jonassachman";
+console.log(jonas);
+
+console.log(
+  `${jonas.firstName} ${jonas.lastName} has 3 friend ${jonas.friends} , the is best friend is ${jonas.friends[0]}.`
+);
