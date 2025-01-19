823956a6bde8595ef7d7a5114dd5b4d81d98a485
avi-codesmith <avi.codesmith@gmail.com>
2025-01-19 16:46:53 +0530

    Hoisting in JS

---
+++ Section 5/lecture-97/script.js
@@ -0,0 +1,43 @@
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = "jonas";
+let job = "teacher";
+const year = 1991;
+
+// In functions
+
+console.log(funcDel(1, 2));
+console.log(funcEx(1, 2));
+console.log(funcExArrow(1, 2));
+
+function funcDel(a, b) {
+  return a + b;
+}
+
+const funcEx = function (a, b) {
+  return a + b;
+};
+
+const funcExArrow = (a, b) => a + b;
+
+// Example
+
+if (!totalCart) {
+  deleteShopCart();
+}
+
+var totalCart = 10;
+
+function deleteShopCart() {
+  console.log("All have gone!");
+}
+
+var x = 10;
+let y = 10;
+const z = 10;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
