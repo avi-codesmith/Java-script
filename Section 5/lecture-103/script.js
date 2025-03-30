af3bad7be64646d328cd2ddf01c4f53d98090622
avi-codesmith <avi.codesmith@gmail.com>
2025-03-30 20:42:05 +0530

    Scope in JS

---
+++ Section 5/lecture-103/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+if (true) {
+  let x = 5;
+  const y = 15;
+  var z = 25;
+}
+
+// console.log(x); //error
+// console.log(y); //error
+console.log(z);
+
+function outer() {
+  let count = 0;
+
+  return function inner() {
+    count++;
+    console.log(count);
+  };
+}
+
+const counter1 = outer();
+counter1(); //1
+counter1(); //2
+
+const counter2 = outer();
+counter2(); //1
+counter2(); //2
+
+function createCounter(start) {
+  return {
+    increment: function () {
+      start++;
+      console.log(start);
+    },
+    decrement: function () {
+      start--;
+      console.log(start);
+    },
+  };
+}
+
+const counter = createCounter(5);
+counter.increment(); //6
+counter.increment(); //7
+counter.decrement(); //6
+
+// Immediately Invoked Function Expression
+
+(function () {
+  var x = 10;
+})();
+
+//console.log(x); // error
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
