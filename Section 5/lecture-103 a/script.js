903e8795de3a3c8860fa9d25ad61783f345e3a6b
avi-codesmith <avi.codesmith@gmail.com>
2025-04-12 18:03:13 +0530

    Coeion in js

---
+++ Section 5/lecture-103 a/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log("Iam" + 23 + "yrs old");
+
+const test = "23" + "2";
+console.log(test);
+console.log(typeof test);
+
+const test2 = 1 + "1";
+console.log(test2);
+console.log(typeof test2);
+
+const test3 = "1" - 1;
+console.log(test3);
+console.log(typeof test3);
+
+const test4 = 2 + 3 + 4 + "5";
+console.log(test4);
+console.log(typeof test4);
